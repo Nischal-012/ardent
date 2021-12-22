12c1b68677493b6f2bd01a310f13a0393678122e
SABINAKSHETTRY <sabinathapakshettri2001@gmail.com>
2021-12-22 18:10:34 +0545

    navbar kek

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open');
-    menuOpen = true;
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
   } else {
-    menuBtn.classList.remove('open');
-    menuOpen = false;
+    x.className = "topnav";
   }
-});
+}
